6dd2f68832cd4009ba9fa70391e1dd201e3c1253
sejong <mmssjj72@naver.com>
2024-01-13 01:13:11 +0900

    BottomSheet 적용

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
 import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
+import 'package:calendar_scheduler/const/color.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import '../component/calendar.dart';
@@ -22,34 +24,41 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          Calendar(
-            selectedDay: selectedDay,
-            focusedDay: focusedDay,
-            onDaySelected: onDaySelected,
-          ),
-          SizedBox(height: 8.0),
-          TodayBanner(selectedDay: selectedDay, scheduleCount: 3),
-          SizedBox(
-            height: 8.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, index) {
-              return ScheduleCard(
-                  startTime: 8,
-                  endTime: 14,
-                  content: "프로그래밍 공부하기",
-                  color: Colors.red);
-            }),
-          ),
-        ],
+      floatingActionButton: renderFloatingActionButton(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Calendar(
+              selectedDay: selectedDay,
+              focusedDay: focusedDay,
+              onDaySelected: onDaySelected,
+            ),
+            SizedBox(height: 8.0),
+            TodayBanner(selectedDay: selectedDay, scheduleCount: 3),
+            SizedBox(
+              height: 8.0,
+            ),
+            _ScheduleList(),
+          ],
+        ),
       ),
-    ));
+    );
+  }
+
+  FloatingActionButton renderFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          builder: (_) {
+            return ScheduleBottomSheet();
+          },
+        );
+      },
+      backgroundColor: PRIMARY_COLOR,
+      child: Icon(Icons.add),
+    );
   }
 
   onDaySelected(DateTime selectedDay, DateTime focusedDay) {
@@ -57,5 +66,30 @@ class _HomeScreenState extends State<HomeScreen> {
       this.selectedDay = selectedDay;
       this.focusedDay = selectedDay;
     });
+  }
+}
+
+class _ScheduleList extends StatelessWidget {
+  const _ScheduleList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: ListView.separated(
+            separatorBuilder: (context, index) {
+              return SizedBox(height: 8.0);
+            },
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return ScheduleCard(
+                  startTime: 8,
+                  endTime: 14,
+                  content: "프로그래밍 공부하기",
+                  color: Colors.red);
+            }),
+      ),
+    );
   }
 }
