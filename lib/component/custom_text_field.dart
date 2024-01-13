f2d5b046056ebad2004e5c933f9b3f5e3387cba6
sejong <mmssjj72@naver.com>
2024-01-13 17:14:32 +0900

    Wraap 위젯 사용하기

---
+++ lib/component/custom_text_field.dart
@@ -0,0 +1,31 @@
+import 'package:calendar_scheduler/const/color.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  const CustomTextField({required this.label, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            color: PRIMARY_COLOR,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        TextField(
+          cursorColor: Colors.grey,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            filled: true,
+            fillColor: Colors.grey[300],
+          ),
+        ),
+      ],
+    );
+  }
+}
